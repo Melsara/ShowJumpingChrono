2e86b2e6e6ca9e7affcdcbab480d06841b895234
Melsara <kaxhortz@gmail.com>
2017-12-19 22:09:24 +0100

    Adding functionalities for +1 penalties, +4 penalties and total penalties calculation.

--- app/src/main/java/com/sardegnaisoladicavalli/example/showjumpingchrono/MainActivity.java
+++ app/src/main/java/com/sardegnaisoladicavalli/example/showjumpingchrono/MainActivity.java
@@ -8,23 +8,15 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String ROW_1 = "Chrono";
-    private static final String ROW_2 = "Course penalties";
-    private static final String ROW_3 = "Time penalties";
-    private static final String ROW_4 = "Total penalties";
-
     CustomChrono mChronometer;
-
     long timeWhenStopped = 0;
     String timeDisplayed;
+    int coursePens = 0;
+    int timePens = 0;
+    int totalPens = 0;
+    final int COURSE_PENS = 4;
+    final int TIME_PEN = 1;
 
-
-/*    private final String[][] DATA_TO_SHOW = {
-            {ROW_1, " ", "0"},
-            {ROW_2, " ", "0"},
-            {ROW_3, " ", "0"},
-            {ROW_4, " ", "0"}
-    };*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +32,8 @@ public class MainActivity extends AppCompatActivity {
      * @param timeDisplayed
      */
     public void displayTimeSet (String timeDisplayed) {
-        TextView scoreView = (TextView) findViewById(R.id.time_set);
-        scoreView.setText(String.valueOf(timeDisplayed));
+        TextView timeView = (TextView) findViewById(R.id.time_set);
+        timeView.setText(String.valueOf(timeDisplayed));
     }
 
     public void startChrono (View view) {
@@ -56,12 +48,54 @@ public class MainActivity extends AppCompatActivity {
         displayTimeSet(timeDisplayed);
     }
 
+    public void displayCoursePens (int coursePens){
+        TextView courseView = (TextView) findViewById(R.id.course_pens);
+        courseView.setText(String.valueOf(coursePens));
+    }
+
+    public void displayTimePens (int timePens){
+        TextView timeView = (TextView) findViewById(R.id.time_pens);
+        timeView.setText(String.valueOf(timePens));
+    }
+
+    public void displayTotalPens (int totalPens){
+        TextView totalView = (TextView) findViewById(R.id.total_pens);
+        totalPens = timePens + coursePens;
+        totalView.setText(String.valueOf(totalPens));
+    }
+
+    public void addCoursePens (View view) {
+        coursePens = coursePens + COURSE_PENS;
+        displayCoursePens(coursePens);
+        displayTotalPens(totalPens);
+
+    }
+
+    public void addTimePens (View view) {
+        timePens = timePens + TIME_PEN;
+        displayTimePens(timePens);
+        displayTotalPens(totalPens);
+
+    }
 
 
 }
 
 
         /*I HAVE DONE MANY TRIALS - UNUSED BIT OF CODE
+
+            private final String[][] DATA_TO_SHOW = {
+            {ROW_1, " ", "0"},
+            {ROW_2, " ", "0"},
+            {ROW_3, " ", "0"},
+            {ROW_4, " ", "0"}
+    };
+
+    private static final String ROW_1 = "Chrono";
+    private static final String ROW_2 = "Course penalties";
+    private static final String ROW_3 = "Time penalties";
+    private static final String ROW_4 = "Total penalties";
+
 
         start.setOnClickListener(new View.OnClickListener() {
 
